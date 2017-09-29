e3d0c4deac3bee17ecd6de6939d7de627c5369a5
John Tumminaro <jtumminaro@flowhub.co>
2017-09-28 21:28:30 -0600

    Update to Perfect-PostgreSQL dependency to version 3.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "PostgresStORM",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", majorVersion: 2),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", majorVersion: 3),
         .Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 1),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-XML.git", majorVersion: 2),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1),
